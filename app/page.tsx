97fb89b9a6bfbef1a10000d386ae9c939643ed52
anuj <anujsbhuyar@gmail.com>
2024-09-18 13:38:33 +0530

    added Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { BackgroundCellAnimation } from "@/components/BackgroundRippleEffect";
 import Features from "@/components/Features";
 import { FloatingNavDemo } from "@/components/Navbar";
+import { Video } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <FloatingNavDemo />
       <BackgroundCellAnimation />
-      {/* <Video /> */}
+      <Video />
       <Features />
     </div>
   );
